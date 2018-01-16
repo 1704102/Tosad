8e6d237f6b72fa232989fd567306e5388a81d083
marti <martijn_bakker@upcmail.nl>
2018-01-16 21:24:29 +0100

    Initial commit

---
+++ src/main/java/com/vogella/jersey/first/LoginResource.java
@@ -0,0 +1,22 @@
+package com.vogella.jersey.first;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Created by marti on 13-6-2017.
+ */
+@Path("login")
+public class LoginResource {
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String Login() {
+
+        return "hello world";
+
+    }
+}
